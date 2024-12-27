0c444d6c1211bcc0c00a2dbd5e036bae39e6e814
Nescogo <nataly.l_94@mail.ru>
2024-12-27 23:11:56 +0300

    Update tar_log.sh

--- HW 2.3 Bash/Task2/tar_log.sh
+++ HW 2.3 Bash/Task2/tar_log.sh
@@ -2,4 +2,4 @@
 
 echo "Start Backup"
 cd /
-sudo tar -czpvf /archive/backup_date+%Y-%m-%d.tar.gz /home /var/log /etc/vsftpd.conf /etc/ssh/sshd_config /etc/xrdp/xrdp.ini
+sudo tar -czpvf /archive/backup_date_`+%Y-%m-%d`.tar.gz /home /var/log /etc/vsftpd.conf /etc/ssh/sshd_config /etc/xrdp/xrdp.ini
